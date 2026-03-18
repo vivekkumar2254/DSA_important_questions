d47a8cb1a894eaf4491ecc82830525483e0d5d0e
vivek  maurya <119119351+vivekkumar2254@users.noreply.github.com>
2026-03-19 01:00:47 +0530

    Time: 11 ms (79.21%), Space: 90.4 MB (82.02%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -4,13 +4,13 @@ public:
           int cnt=0;
           for(int i=0;i<grid.size();i++){
               for(int j=0;j<grid[0].size();j++){
-                      if(i-1>=0){
+                      if(i>0){
                         grid[i][j]+=grid[i-1][j];
                       }
-                      if(j-1>=0){
+                      if(j>0){
                         grid[i][j]+=grid[i][j-1];
                       }
-                      if(i-1>=0 && j-1>=0){
+                      if(i>0 && j>0){
                         grid[i][j]-=grid[i-1][j-1];
                       }
 
