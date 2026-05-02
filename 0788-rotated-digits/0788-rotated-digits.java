54aa4ed4a99d7c1e814dfd3daba7aff061eb223a
vivek  maurya <119119351+vivekkumar2254@users.noreply.github.com>
2026-05-02 23:54:00 +0530

    Time: 3 ms (91.63%), Space: 41.9 MB (82.58%) - LeetHub

---
+++ 0788-rotated-digits/0788-rotated-digits.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    private boolean isGood(int num) {
+        boolean changed = false;
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            if (digit == 3 || digit == 4 || digit == 7) return false;
+            if (digit == 2 || digit == 5 || digit == 6 || digit == 9) changed = true;
+
+            num /= 10;
+        }
+        return changed;
+    }
+
+    public int rotatedDigits(int n) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (isGood(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
